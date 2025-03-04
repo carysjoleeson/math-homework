8668b051a34d78ba9c9c9c7cc932ea0ad3142cd5
carysjoleeson <carysjoleeson@gmx.com>
2025-03-04 17:51:19 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Enter a number: ";
+    int n;
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << "The number is even.";
+    } else {
+        cout << "The number is odd.";
+    }
+    return 0;
+}
